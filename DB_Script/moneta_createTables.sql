7c8a75eec082012b8d180304d5459966c1cca90e
gabrieltalih <78610215+gabrieltalih@users.noreply.github.com>
2023-01-21 20:18:42 -0500

    Update moneta_createTables.sql

--- DB_Script/moneta_createTables.sql
+++ DB_Script/moneta_createTables.sql
@@ -2,15 +2,15 @@ create database moneta_db;
 use moneta_db;
 
 create table users(
-user_id INT primary key,
-first_name varchar(20),
-last_name varchar(40),
-email varchar(50),
-date_of_birth date
+    user_id INT primary key,
+    first_name varchar(20),
+    last_name varchar(40),
+    email varchar(50),
+    date_of_birth date
 );
 
 create table transactions(
-	transaction_id int primary key,
+    transaction_id int primary key,
     total_spent decimal(10,2) not null,
     transaction_date date,
     company varchar(50),
@@ -20,15 +20,13 @@ create table transactions(
 );
 
 create table categories (
-
-	category_id int primary key,
+    category_id int primary key,
     category_name varchar(60)
 );
 
 create table receipts (
-
-receipt_num int primary key,
-screenshot BLOB
+    receipt_num int primary key,
+    screenshot BLOB
 );
 
 
