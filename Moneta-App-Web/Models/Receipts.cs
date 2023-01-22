538959ad8512624cbc10cebd08377fac6ebc3aed
cedparadis99@gmail.com <cedparadis99@gmail.com>
2023-01-21 21:21:14 -0500

    Models and pages created (not tested)

---
+++ Moneta-App-Web/Models/Receipts.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Moneta_App_Web.Models
+{
+    public class Receipts
+    {
+        [Key]
+        public int receipt_num { get; set; }
+
+        [Required]
+        public string screenshot { get; set; }
+    }
+}
---
+++ Moneta-App-Web/Pages/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Moneta_App_Web.Pages
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
