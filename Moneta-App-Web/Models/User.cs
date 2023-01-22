d9a382feebf009c905ccc5f442ce682433079b54
cedparadis99@gmail.com <cedparadis99@gmail.com>
2023-01-21 20:14:09 -0500

    Boilerplate code completed

---
+++ Moneta-App-Web/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Moneta_App_Web.Models
+{
+    public class Users
+    {
+        [Key]
+        public int user_id { get; set; }
+        [Required]
+        public string?  first_name { get; set; }
+        [Required]
+        public string? last_name { get; set; }
+        [Required]
+        public string? email { get; set; }
+        [Required]
+        public DateTime date_of_birth { get; set; }
+
+    }
+}
---
+++ Moneta-App-Web/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Moneta_App_Web.Data;
+using Moneta_App_Web.Models;
+
+namespace Moneta_App_Web.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    private readonly ApplicationDbContext _context;
+    public IEnumerable<Users> Users { get; set; }
+    public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public void OnGet()
+    {
+        Users = _context.Users;
+    }
+}
